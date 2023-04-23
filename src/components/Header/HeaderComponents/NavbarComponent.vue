0322d8a69d390ce0e52eeb4398478b00664a7187
Thiago Barros <thiago.barros@bobby.com.br>
2023-04-23 15:14:01 -0300

    feat: create header child component

--- src/components/Header/HeaderComponents/NavbarComponent.vue
+++ src/components/Header/HeaderComponents/NavbarComponent.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
-        <nav>
-            <h1>This is the navbar component</h1>
-        </nav>
+        <div class="navbar">
+            <div class="max-width">
+                <div class="logo"><a href="#">Port<span>folio</span></a></div>
+                <ul class="menu">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#skills">Stacks</a></li>
+                    <li><a href="#teams">MasterMind</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'NavbarComponent',
 }
 </script>
