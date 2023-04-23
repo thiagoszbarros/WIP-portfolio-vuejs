6494d5880c4681dd23db7ba3befb7bd5e4dd6eb6
Thiago Barros <thiago.barros@bobby.com.br>
2023-04-23 15:17:34 -0300

    chore: remove deprecated text

--- src/components/Header/HeaderComponents/NavbarComponent.vue
+++ src/components/Header/HeaderComponents/NavbarComponent.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="navbar">
             <div class="max-width">
-                <div class="logo"><a href="#">Port<span>folio</span></a></div>
+                <div class="logo"><a href="#"><span></span></a></div>
                 <ul class="menu">
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
