b022fdd7ae839aa505c66a479fb1b62721002e7a
Thiago Barros <thiago.barros@bobby.com.br>
2023-04-23 15:13:51 -0300

    feat: add child component into header component

--- src/components/Header/HeaderComponent.vue
+++ src/components/Header/HeaderComponent.vue
@@ -1,20 +1,17 @@
 <template>
     <header>
-        <h1>This is the header component</h1>
-        <div><NavbarComponent /></div>
-        <ProfilePictureComponent />
+        <div>
+            <NavbarComponent />
+        </div>
     </header>
 </template>
 
 <script>
 import NavbarComponent from './HeaderComponents/NavbarComponent.vue';
-import ProfilePictureComponent from './HeaderComponents/ProfilePictureComponent.vue';
 
 export default {
-    name: 'HeaderComponent',
     components: {
         NavbarComponent,
-        ProfilePictureComponent,
     }
 }
 </script>
